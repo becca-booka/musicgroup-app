f8e2dca72cfd8b2999c6a9b5ddeca8441e7b5497
BeccaNaude <beccanaude@gmail.com>
2023-09-25 16:41:11 +0200

    took out django secret key

--- musicgroup/musicgroup/settings.py
+++ musicgroup/musicgroup/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z4^tjlz)34i$k&kph*y5u_*5bnn_yl45rn5@khqx5$%39ri&qk'
+SECRET_KEY = **** 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
